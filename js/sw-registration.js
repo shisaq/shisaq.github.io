27bcf13b6509f80fbc0b5f5f16540bd36b751329
shisaq <shisaq@gmail.com>
2021-03-10 11:16:40 -0500

    locale: change alert to Chinese

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -48,8 +48,8 @@ if(navigator.serviceWorker){
     if(data.command == "UPDATE_FOUND"){
       console.log("UPDATE_FOUND_BY_SW", data);
       createSnackbar({
-        message: "Content updated.",
-        actionText:"refresh",
+        message: "内容已更新。",
+        actionText:"刷新",
         action: function(e){location.reload()}
       })
     }
